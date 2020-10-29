a84e7d53426a277b391ebb5909c61b40f791b74e
bellothornus <damia_iv@yahoo.es>
2020-10-29 17:08:21 +0100

    fix(Range_Extraction): :bug: we have solved the series with length of 2 or less, now print the correct answer

--- src/code.py
+++ src/code.py
@@ -2,10 +2,18 @@ def solution(args):
     #necesito recorrer el array buscando una sucesión de números para añadirlos a una cadena de texto
     #viendo si tienen como mínimo una sucesión de 3 integers y ponerlos así 1-3 incluyendo ambos
     #si no, simplemente me printeas el integer sea este negativo o positivo
+    #hay un problema y es cuando hay pocos numeros en una serie CHCKED
+    #se puede refactorizar quitando la variable last_number_range, es uso de memoria redundante
+    #se puede refactorizar cambiando la variable first_number_range por un acumulador para buscar el indice del mismo
+    #se puede refactorizar haciendo qu el condicional tercer que comprueba contenido de la serie se ponga en segundo lugar
     check,first_number_range,last_number_range = 0,0,0
     answer = ""
     for arg in range(len(args)):
         if arg == len(args)-1:
+            break
+        #este condicional si es la ultima serie del array
+        if len(args) == args[arg+1] and check >= 1:
+            answer += str(first_number_range) + "-" + str(args[arg+1])
             break
         # este condicional mira si es el principio de una serie
         if args[arg+1] == args[arg]+1 and check == 0:
@@ -20,10 +28,15 @@ def solution(args):
         elif args[arg+1] == args[arg]+1 and check >= 1:
             check +=1
             continue
+        #ideas
+        elif args[arg+1] != args[arg]+1 and check == 1:
+            answer += str(first_number_range) + "," + str(args[arg])
+            check = 0
+        #fin ideas
         else:
             answer += str(args[arg]) + ","
     return answer
 
 if __name__ == "__main__":
-    assert solution([-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20])
-    assert solution([[-3,-2,-1,2,10,15,16,18,19,20]])
+    assert solution([-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20] == '-6,-3-1,3-5,7-11,14,15,17-20')
+    assert solution([-3,-2,-1,2,10,15,16,18,19,20] == '-3--1,2,10,15,16,18-20')
