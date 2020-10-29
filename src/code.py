3912a90c3e51538e3460d8cafa9cabfdc9a46cb9
bellothornus <damia_iv@yahoo.es>
2020-10-29 17:48:07 +0100

    fix(Range_Extraction): :bug: now solves if the last value is not forming part of any series

--- src/code.py
+++ src/code.py
@@ -9,30 +9,32 @@ def solution(args):
     check,first_number_range,last_number_range = 0,0,0
     answer = ""
     for arg in range(len(args)):
-        if arg == len(args)-1:
-            break
         #este condicional si es la ultima serie del array
-        if len(args) == args[arg+1] and check >= 1:
-            answer += str(first_number_range) + "-" + str(args[arg+1])
+        if len(args) == arg+1 and check >= 1:
+            answer += str(first_number_range) + "-" + str(args[arg])
+            break
+        #esto es para romper el bucle si el indice se pasa
+        if len(args) == arg+1:
+            answer += str(args[arg])
             break
         # este condicional mira si es el principio de una serie
         if args[arg+1] == args[arg]+1 and check == 0:
             first_number_range = args[arg]
             check += 1
+        #este condicional mira si entra dentro del rango de una serie
+        elif args[arg+1] == args[arg]+1 and check >= 1:
+            check +=1
+            continue   
         #este condicional mira si es el final de una serie válida
         elif args[arg+1] != args[arg]+1 and check >= 2:
             last_number_range = args[arg]
             check = 0
             answer += str(first_number_range) + "-" + str(last_number_range) + ","
-        #este condicional mira si entra dentro del rango de una serie
-        elif args[arg+1] == args[arg]+1 and check >= 1:
-            check +=1
-            continue
-        #ideas
+        #esto es si no hacen series de 3 (es decir de 2)
         elif args[arg+1] != args[arg]+1 and check == 1:
-            answer += str(first_number_range) + "," + str(args[arg])
+            answer += str(first_number_range) + "," + str(args[arg]) + ","
             check = 0
-        #fin ideas
+        #esto es si no hay ninguna serie
         else:
             answer += str(args[arg]) + ","
     return answer
@@ -40,3 +42,4 @@ def solution(args):
 if __name__ == "__main__":
     assert solution([-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20]) == '-6,-3-1,3-5,7-11,14,15,17-20'
     assert solution([-3,-2,-1,2,10,15,16,18,19,20]) == '-3--1,2,10,15,16,18-20'
+    assert solution([1,2,3,0]) == '1-3,0'
