491cfd70fc13998f45de7a7adcb14bc43549acbe
bellothornus <damia_iv@yahoo.es>
2020-10-29 17:54:37 +0100

    fix(Range_Extraction): :bug: pytest tests solved incorrect syntaxis

--- test/test_general.py
+++ test/test_general.py
@@ -7,16 +7,16 @@ def test_general():
     assert solution([[-3,-2,-1,2,10,15,16,18,19,20]])
 
 def test_positive_numbers():
-    assert solution([1,2,3,4,5,6,7] == "1-7")
+    assert solution([1,2,3,4,5,6,7])== "1-7"
 
 def test_negative_numbers():
-    assert solution([-6,-5,-4,-3,-2,-1] == "-6--1")
+    assert solution([-6,-5,-4,-3,-2,-1]) == "-6--1"
 
 def test_from_negative_to_positive():
-    assert solution([-3,-2,-1,0,1,2,3,4] == "-3-4")
+    assert solution([-3,-2,-1,0,1,2,3,4]) == "-3-4"
 
 def test_different_groups():
-    assert solution([1,2,3,4,6,7,8,10,11,12,13,14] == "1-4,6-8,10-14")
+    assert solution([1,2,3,4,6,7,8,10,11,12,13,14]) == "1-4,6-8,10-14"
 
 def test_last_is_single():
     assert solution([1,2,3,0]) == '1-3,0'
