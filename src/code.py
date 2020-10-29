8c2ea27da715f4785f7559e22eb477503c318760
bellothornus <damia_iv@yahoo.es>
2020-10-29 17:50:46 +0100

    perf(Range_Extraction): :zap: less use of redundant variables
    the variable "last_number_range" is removed and used "args[arg]" instead

--- src/code.py
+++ src/code.py
@@ -3,10 +3,10 @@ def solution(args):
     #viendo si tienen como mínimo una sucesión de 3 integers y ponerlos así 1-3 incluyendo ambos
     #si no, simplemente me printeas el integer sea este negativo o positivo
     #hay un problema y es cuando hay pocos numeros en una serie CHCKED
-    #se puede refactorizar quitando la variable last_number_range, es uso de memoria redundante
+    #se puede refactorizar quitando la variable last_number_range, es uso de memoria redundante CHECKED 
     #se puede refactorizar cambiando la variable first_number_range por un acumulador para buscar el indice del mismo
     #se puede refactorizar haciendo qu el condicional tercer que comprueba contenido de la serie se ponga en segundo lugar
-    check,first_number_range,last_number_range = 0,0,0
+    check,first_number_range = 0,0
     answer = ""
     for arg in range(len(args)):
         #este condicional si es la ultima serie del array
@@ -27,9 +27,8 @@ def solution(args):
             continue   
         #este condicional mira si es el final de una serie válida
         elif args[arg+1] != args[arg]+1 and check >= 2:
-            last_number_range = args[arg]
             check = 0
-            answer += str(first_number_range) + "-" + str(last_number_range) + ","
+            answer += str(first_number_range) + "-" + str(args[arg]) + ","
         #esto es si no hacen series de 3 (es decir de 2)
         elif args[arg+1] != args[arg]+1 and check == 1:
             answer += str(first_number_range) + "," + str(args[arg]) + ","
